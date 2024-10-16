e7d25493f73b15276a0e8365620e3ade3c9b401a
Belchenkov <u608110@gmail.com>
2024-10-16 14:02:13 +0400

    Make HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/jobs', [JobController::class, 'index'])->name('jobs.index');
 Route::get('/jobs/create', [JobController::class, 'create'])->name('jobs.create');
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
