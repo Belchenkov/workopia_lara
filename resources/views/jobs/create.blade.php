0180ce1317b495560c51c4401245f22fa1861772
Belchenkov <u608110@gmail.com>
2024-10-18 18:33:33 +0400

    Add Layouts

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,15 +1,9 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create New Job</title>
-</head>
-<body>
-    <h1>Create New Job</h1>
+@extends('layout')
 
+@section('title', 'Create Job')
+
+@section('content')
+    <h1>Create New Job</h1>
     <form action="/jobs" method="POST">
         @csrf
         <input
@@ -24,5 +18,4 @@
         />
         <button type="submit">Submit</button>
     </form>
-</body>
-</html>
+@endsection
---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>Available Jobs</h1>
+@endsection
