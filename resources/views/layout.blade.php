b3010f5031cc45b14de7309ad724a999dce88dcb
Belchenkov <u608110@gmail.com>
2024-10-25 10:15:41 +0400

    Make Hero component & add images

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,13 +8,17 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
           integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
           crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
     <title>{{ $title ?? 'Workopia | Find and list jobs' }}</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100">
-<x-header />
-<main class="container mx-auto p-4 mt-4">
-    {{ $slot }}
-</main>
+    <x-header />
+    @if(request()->is('/'))
+        <x-hero />
+    @endif
+    <main class="container mx-auto p-4 mt-4">
+        {{ $slot }}
+    </main>
 </body>
 </html>
