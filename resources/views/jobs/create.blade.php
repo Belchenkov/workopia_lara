06c830258f9100b8741f00f7a3c89cdd7238d702
Алексей Бельченков <u608110@gmail.com>
2024-11-14 13:19:02 +0400

    File component

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -71,6 +71,8 @@
             <x-inputs.text id="contact_email" name="contact_email" label="Contact Email" placeholder="Enter Contact Email"
                            type="email" />
 
+            <x-inputs.file id="company_logo" name="company_logo" label="Company Logo" />
+
             <button type="submit"
                     class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
                 Save
