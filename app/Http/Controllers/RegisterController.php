4d7f57a6512e81682594f6c8d25e3d27b4b10994
Алексей Бельченков <u608110@gmail.com>
2024-11-19 13:31:33 +0400

    Register new user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
+use App\Repositories\UserRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    public function __construct(
+        private readonly UserRepository $r_user
+    )
+    {}
+
     /*
      * @desc Show register form
      * @route GET /register
@@ -13,5 +21,17 @@ class RegisterController extends Controller
     public function register()
     {
         return view('auth.register');
+    }
+
+    public function store(RegisterRequest $request): \Illuminate\Http\RedirectResponse
+    {
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($validated['password']);
+
+        $this->r_user->create($validated);
+
+        return redirect()
+            ->route('auth.login')
+            ->with('success', 'You are registered and can log in!');
     }
 }
---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserRepository
+{
+    public function all(): Collection
+    {
+        return User::all();
+    }
+
+    public function create(array $data): User
+    {
+        return User::create($data);
+    }
+
+    public function update(int $id, array $data): int
+    {
+        return User::where('id', $id)->update($data);
+    }
+
+    public function delete(int $id): int
+    {
+        return User::destroy($id);
+    }
+}
